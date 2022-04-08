146ae5c98d029ea288186a1880950fe49ffeb16b
unknown <kdmmanapul@gmail.com>
2022-04-08 22:12:17 +0800

    fix: legacy updates

--- server/modules/messages/help.js
+++ server/modules/messages/help.js
@@ -3,10 +3,10 @@ exports.initialize = function(io) {
     exports.use = function(args, initiator) {
         MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "---- Available commands :", "action");
         MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/changePassword [old] [new]", "action");
-        MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/findUser [username]", "action");
-        MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/givegold [username] [amount]", "action");
-        MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/joinparty [username]", "action");
-        MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/leaveparty", "action");
+        // MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/findUser [username]", "action");
+        // MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/givegold [username] [amount]", "action");
+        // MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/joinparty [username]", "action");
+        // MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/leaveparty", "action");
         MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/pa [message]", "action");
         MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "/w [username] [message]", "action");
 
